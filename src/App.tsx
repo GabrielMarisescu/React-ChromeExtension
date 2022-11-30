79e6e05c14ec3e2b6d83c98f86f1d7a78b8cffda
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-11-30 21:46:15 +0100

    Init Project, jest,zod, TS, npm watch, tailwind, vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  console.log(chrome);
+
+  console.log(count);
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      nofuckyouo
+    </>
+  );
+}
+
+export default App;
