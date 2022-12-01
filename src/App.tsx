43021479ce6b8c58984836ce85a2411bd56b77d8
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-12-01 22:02:06 +0100

    Added .env & notion SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   console.log(chrome);
-
-  console.log(count);
+  console.log(import.meta.env);
 
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      nofuckyouo
     </>
   );
 }
