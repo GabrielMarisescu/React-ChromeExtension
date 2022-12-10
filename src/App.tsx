7a2ff44454958bc7e471f80823722cdf17a469ca
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-12-10 17:37:42 +0100

    Revert "Added .env & notion SDK"
    This reverts commit 43021479ce6b8c58984836ce85a2411bd56b77d8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   console.log(chrome);
-  console.log(import.meta.env);
+
+  console.log(count);
 
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      nofuckyouo
     </>
   );
 }
