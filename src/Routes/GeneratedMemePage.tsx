07f76ac3a70a8c47cb65d928a43bcf1f9da75fb0
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-12-24 00:26:27 +0100

    Typed functions in GenerateMemePage & implemented ViewInBrowser func

--- src/Routes/GeneratedMemePage.tsx
+++ src/Routes/GeneratedMemePage.tsx
@@ -17,23 +17,24 @@ function GeneratedMemePageWithData() {
   );
   return <Component />;
 }
-const downloadMeme = () => {};
+const downloadMeme = (): void => {};
 
-const copyToClipboard = (captionedURL: string) => {
+const copyToClipboard = (captionedURL: string): void => {
   navigator.clipboard.writeText(captionedURL);
   //TODO If successful trigger pop up that it's  ok, for 2 seconds
 };
-const viewMemeInBrowser = () => {};
 
 function GeneratedMemePage({ data }: { data: string }) {
   return (
-    //TODO : Copy to clipboard, view in browser, download image
     //TODO : Maybe make a big version of showcasememeimage
     <>
-      <Button title={DOWNLOAD} onClick={downloadMeme} />
-      <Button title={COPYTOCLIPBOARD} onClick={() => copyToClipboard(data)} />
-      <Button title={VIEWINBROWSER} onClick={viewMemeInBrowser} />
-      <section className=' m-auto w-1/2 '>
+      <section className=' m-auto w-1/2 flex '>
+        <Button title={DOWNLOAD} onClick={downloadMeme} />
+        <Button title={COPYTOCLIPBOARD} onClick={() => copyToClipboard(data)} />
+        <a href={data} type='image' target={'_blank'}>
+          <Button title={VIEWINBROWSER} />
+        </a>
+        ;
         <ShowcaseMemeImage imageLink={data} />
       </section>
     </>
