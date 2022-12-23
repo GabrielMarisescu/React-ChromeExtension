4e3cfab1c823d45433439ce63e263507276117d3
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-12-24 00:16:00 +0100

    implemented copyToClipboard

--- src/Routes/GeneratedMemePage.tsx
+++ src/Routes/GeneratedMemePage.tsx
@@ -1,5 +1,7 @@
 import { useAtom } from 'jotai';
+import Button from '../components/DumbComponents/Button';
 import ShowcaseMemeImage from '../components/DumbComponents/ShowcaseMemeImage';
+import { COPYTOCLIPBOARD, DOWNLOAD, VIEWINBROWSER } from '../constants';
 import { template_ID } from '../MainStore';
 import withMemeData from '../utils/HOCs/withMemeData';
 import { usePostMeme } from '../utils/hooks/usePostMeme';
@@ -15,14 +17,22 @@ function GeneratedMemePageWithData() {
   );
   return <Component />;
 }
+const downloadMeme = () => {};
 
-//TODO make props type
+const copyToClipboard = (captionedURL: string) => {
+  navigator.clipboard.writeText(captionedURL);
+  //TODO If successful trigger pop up that it's  ok, for 2 seconds
+};
+const viewMemeInBrowser = () => {};
 
 function GeneratedMemePage({ data }: { data: string }) {
   return (
     //TODO : Copy to clipboard, view in browser, download image
     //TODO : Maybe make a big version of showcasememeimage
     <>
+      <Button title={DOWNLOAD} onClick={downloadMeme} />
+      <Button title={COPYTOCLIPBOARD} onClick={() => copyToClipboard(data)} />
+      <Button title={VIEWINBROWSER} onClick={viewMemeInBrowser} />
       <section className=' m-auto w-1/2 '>
         <ShowcaseMemeImage imageLink={data} />
       </section>
