dd77befe14510dc05cc007deedbd724deb618c3b
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2023-01-01 13:13:22 +0100

    refactored to react hook form & inputTextDeleted

--- src/components/DisplayMemeInputs.tsx
+++ src/components/DisplayMemeInputs.tsx
@@ -1,7 +1,8 @@
-import { useAtom } from 'jotai';
-import React, { useState } from 'react';
-import { inputBoxesValues, isGenerateButtonDisaled } from '../MainStore';
+import { useForm } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
 import InputText from './DumbComponents/InputText';
+import { inputBoxesValues, isGenerateButtonDisaled } from '../MainStore';
+import { useAtom } from 'jotai';
 
 interface DisplayMemeInputsProps {
   numberOfBoxesToCaption: number;
@@ -10,35 +11,53 @@ interface DisplayMemeInputsProps {
 function DisplayMemeInputs({
   numberOfBoxesToCaption,
 }: DisplayMemeInputsProps): JSX.Element {
-  const [_, setInputValue] = useState<string>('');
-  const [__, setIsButtonDisabled] = useAtom(isGenerateButtonDisaled);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [_, setIsButtonDisabled] = useAtom(isGenerateButtonDisaled);
   const [inputBoxes, setInputBoxes] = useAtom(inputBoxesValues);
   const arrayOfBoxes: string[] = Array(numberOfBoxesToCaption).fill('');
 
-  const updateCaption = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
+  const updateCaption = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputBoxes((prevInputBoxes) => {
-      setInputValue(e.target.value);
       const newInputBoxes = [...prevInputBoxes];
-      newInputBoxes[i] = e.target.value;
+      const index = parseInt(e.target.name.split('-')[1], 10);
+      newInputBoxes[index] = e.target.value;
       return newInputBoxes;
     });
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     arrayOfBoxes.length !== inputBoxes.length
       ? setInputBoxes(arrayOfBoxes)
       : null;
-    console.log(inputBoxes, arrayOfBoxes);
-    const isSomeInputFilled = inputBoxes.some((input) => input === '');
+
+    const isSomeInputFilled = inputBoxes.every((input) => input === '');
     setIsButtonDisabled(isSomeInputFilled);
-  }, [inputBoxes, numberOfBoxesToCaption]);
+  }, [numberOfBoxesToCaption, inputBoxes]);
+
+  const onSubmit = (data: unknown) => {
+    console.log(data);
+    //reset();
+  };
 
   return (
     <>
-      <form className='flex flex-col'>
-        {arrayOfBoxes.map((_, i) => (
-          <InputText onChange={(e) => updateCaption(e, i)} key={i} />
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
+        {inputBoxes.map((_, i) => (
+          <input
+            {...register(`memeInput${i}`)}
+            id='MemeInputBox'
+            type='text'
+            autoComplete='off'
+            placeholder='Meme Caption'
+            className=' placeholder:text-white border-blue-500 self-center my-1 w-4/5 h-10 border-2 text-white bg-transparent px-5 pr-16 rounded-lg focus:outline-none'
+          />
         ))}
+        <button type='submit'> Submit</button>
       </form>
     </>
   );
